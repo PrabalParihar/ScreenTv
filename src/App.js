4c684334e4f9be2106bc1e0f67bb779d0138c91a
Prabal Pratap Singh <prabalparihar95@gmail.com>
2022-03-06 16:45:41 +0530

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Movies from './components/Movies';
+import Favourites from './components/Favourites';
+
+import {BrowserRouter as Router,Routes ,Route , BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+     <Routes>
+        <Route exact  path='/'  element={
+          <>
+            <Banner />
+            <Movies />
+          </>
+        }/>
+        <Route path='/favourites' element={<Favourites/>} />
+        </Routes>
+      
+      {/* <Banner/> */}
+      {/* <Movies/> name="udai" */}
+      {/* <Favourite/> */}
+    </Router>
   );
 }
 
